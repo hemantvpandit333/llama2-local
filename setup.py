f074e796020914f3defb16f39333a89548677292
hemantvpandit333 <hemantvpandit333@gmail.com>
2024-04-03 10:26:44 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Llama2 project on local machine',
+    version= '0.0.0',
+    author= 'HVP',
+    author_email= 'hemantvpandit333@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
